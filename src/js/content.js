a33b1fbb4816551cb9e6d1bea2a4f1e5d0542d16
yutianriyue <15634988823@163.com>
2017-01-05 14:47:03 +0800

    第一次递交

---
+++ src/js/content.js
@@ -0,0 +1,4 @@
+module.exports = " <br> It works from content.js.2222" +
+    "俺是中国人</br>"
+
+
---
+++ src/js/entry.js
@@ -0,0 +1,4 @@
+require("../css/test.css");
+document.write("It works1111111111111111111efefef");
+document.write(require("./content.js"));
+// require("!style!css!../css/test.css");
